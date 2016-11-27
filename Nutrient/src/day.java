09a56888efca01696e81ca656d5234f279b3d8d7
teamnutriomics <teamnutriomics@gmail.com>
2016-11-27 01:06:13 -0500

    Add files via upload

---
+++ Nutrient/src/day.java
@@ -0,0 +1,67 @@
+
+import java.awt.Toolkit;
+import java.lang.reflect.Array;
+
+import javax.swing.*;
+
+
+
+public class day{
+	
+
+
+
+	static micronutrient vitaminA = new micronutrient("Vitamin A");
+	static micronutrient vitaminC = new micronutrient("Vitamin C");
+	static micronutrient calcium = new micronutrient("Calcium");
+	static micronutrient iodine = new micronutrient("Iodine");
+	static micronutrient folate = new micronutrient("Folate");
+	static micronutrient zinc = new micronutrient("Zinc");
+	static micronutrient iron = new micronutrient("Iron");
+
+	
+	static double vitaminAConsumed, vitaminCConsumed, calciumConsumed, iodineConsumed, folateConsumed, zincConsumed, ironConsumed;
+	
+	static micronutrient[] consumed = {vitaminA, vitaminC, calcium, iodine, folate, zinc, iron};
+
+
+	public static void main(String[] args) {
+
+		
+
+		//Input your consumption values here
+		vitaminAConsumed = 0;
+		vitaminCConsumed = 0;
+		calciumConsumed = 0;
+		iodineConsumed = 0;
+		folateConsumed = 0;
+		zincConsumed = 0;
+		ironConsumed = 0;
+		
+		day();
+	
+	}
+	
+	
+
+	public static void day(){
+		vitaminA.amountConsumed = vitaminAConsumed;
+		vitaminC.amountConsumed = vitaminCConsumed;
+		calcium.amountConsumed = calciumConsumed;
+		iodine.amountConsumed = iodineConsumed;
+		folate.amountConsumed = folateConsumed;
+		zinc.amountConsumed = zincConsumed;
+		iron.amountConsumed = ironConsumed;
+		
+		for(micronutrient a : consumed){
+			if(a.amountConsumed < a.dailyMin)
+				System.out.println("You aren't consuming enough " + a.name + ". You should consume more " + a.increase + "!");
+			else if (a.amountConsumed > a.dailyMax)
+				System.out.println("You are consuming too much " + a.name + ". You should consume fewer " + a.name + "-rich foods!");
+			else
+				System.out.println("You are consuming a suitable amount of " + a.name + ". Keep up the great nutrition!");
+
+		}
+	}
+	
+}
---
+++ Nutrient/src/micronutrient.java
@@ -0,0 +1,62 @@
+
+public class micronutrient {
+
+	double dailyMin;
+	double dailyMax;
+	
+	public double amountConsumed;
+	public String increase;
+	public String name;
+	public micronutrient(String mn){
+		
+		name = mn;
+		if(name.equals("Vitamin A")){
+			dailyMin = .9;
+			dailyMax = 3;
+			increase = "Daraba (Sweet Potatoes)";
+		}
+		else if(name.equals("Vitamin C")){
+			dailyMin = 90;
+			dailyMax = 2000;
+			increase = "Citrus Fruits";
+		}
+		else if(name.equals("Calcium")){
+			dailyMin = 1000;
+			dailyMax = 2500;
+			increase = "Okra Gumbo or Citrus Fruits";
+		}
+		else if(name.equals("Iodine")){
+			dailyMin = .15;
+			dailyMax = 1.1;
+			increase = "Okra Gumbo or Peanut Butter";
+
+		}
+		else if(name.equals("Folate")){
+			dailyMin = .4;
+			dailyMax = 1;
+			increase = "Okra Gumbo, Peanut Butter, or Green Beans";
+
+		}
+		else if(name.equals("Zinc")){
+			dailyMin = 11;
+			dailyMax = 40;
+			increase = "Beef";
+
+		}
+		else if(name.equals("Iron")){
+			dailyMin = 8;
+			dailyMax = 45;
+			increase = "Green Beans or Pork";
+
+		}
+		
+		amountConsumed = 0;
+		
+	}
+	
+	public void setConsumed(double val){
+		amountConsumed = val;
+		
+	}
+	
+}
